3809d6197a6e2bd770ca3da2f0348253cc3558d3
Raz <155845276+Raaazeeeruuu@users.noreply.github.com>
2024-05-23 00:06:15 +0800

    Add files via upload

--- Physics/sub.py
+++ Physics/sub.py
@@ -65,82 +65,83 @@ class ProjectileMotion(SubWindow):
         self.velocity_container.place(anchor="n", relx=0.5, rely=0.1, relwidth=1, relheight=0.225)
 
         self.velocity_label = tk.Label(self.velocity_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Velocity : ")
-        self.velocity_label.place(anchor="center", relx=0.25, rely=0.3)
+        self.velocity_label.place(anchor="center", relx=0.25, rely=0.5)
 
         self.velocity_entry = tk.Entry(self.velocity_container, font=text.P2, fg=colors.grey, bg=colors.white, relief="flat", justify="center")
-        self.velocity_entry.place(anchor="center", relx=0.525, rely=0.3, relwidth=0.35, relheight=0.325)
+        self.velocity_entry.place(anchor="center", relx=0.525, rely=0.5, relwidth=0.35, relheight=0.325)
 
         self.velocity_unit = tk.Label(self.velocity_container, font=text.P1, fg=colors.white, bg=colors.grey, text="m/s")
-        self.velocity_unit.place(anchor="center", relx=0.75, rely=0.3)
-
-        self.velocity_decrease_button = tk.Button(self.velocity_container, font=text.P2, fg=colors.grey, bg=colors.white, text="<")
-        self.velocity_decrease_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.velocity_decrease_button.place(anchor="center", relx=0.38, rely=0.7, relwidth=0.075)
-
-        self.velocity_increase_button = tk.Button(self.velocity_container, font=text.P2, fg=colors.grey, bg=colors.white, text=">")
-        self.velocity_increase_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.velocity_increase_button.place(anchor="center", relx=0.67, rely=0.7, relwidth=0.075)
+        self.velocity_unit.place(anchor="center", relx=0.75, rely=0.5)
         
-        self.weight_container = tk.Canvas(self.control_panel, bg=colors.grey, highlightthickness=0)
-        self.weight_container.place(anchor="n", relx=0.5, rely=0.325, relwidth=1, relheight=0.225)
+        self.distance_container = tk.Canvas(self.control_panel, bg=colors.grey, highlightthickness=0)
+        self.distance_container.place(anchor="n", relx=0.5, rely=0.325, relwidth=1, relheight=0.225)
 
-        self.weight_label = tk.Label(self.weight_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Weight : ")
-        self.weight_label.place(anchor="center", relx=0.25, rely=0.3)
+        self.distance_label = tk.Label(self.distance_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Distance : ")
+        self.distance_label.place(anchor="center", relx=0.25, rely=0.5)
 
-        self.weight_entry = tk.Entry(self.weight_container, font=text.P2, fg=colors.grey, bg=colors.white, relief="flat", justify="center")
-        self.weight_entry.place(anchor="center", relx=0.525, rely=0.3, relwidth=0.35, relheight=0.325)
+        self.distance_entry = tk.Entry(self.distance_container, font=text.P2, fg=colors.grey, bg=colors.white, relief="flat", justify="center")
+        self.distance_entry.place(anchor="center", relx=0.525, rely=0.5, relwidth=0.35, relheight=0.325)
 
-        self.weight_unit = tk.Label(self.weight_container, font=text.P1, fg=colors.white, bg=colors.grey, text="kg")
-        self.weight_unit.place(anchor="center", relx=0.75, rely=0.3)
-
-        self.weight_decrease_button = tk.Button(self.weight_container, font=text.P2, fg=colors.grey, bg=colors.white, text="<")
-        self.weight_decrease_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.weight_decrease_button.place(anchor="center", relx=0.38, rely=0.7, relwidth=0.075)
-
-        self.weight_increase_button = tk.Button(self.weight_container, font=text.P2, fg=colors.grey, bg=colors.white, text=">")
-        self.weight_increase_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.weight_increase_button.place(anchor="center", relx=0.67, rely=0.7, relwidth=0.075)
+        self.distance_unit = tk.Label(self.distance_container, font=text.P1, fg=colors.white, bg=colors.grey, text="kg")
+        self.distance_unit.place(anchor="center", relx=0.75, rely=0.5)
 
         self.angle_container = tk.Canvas(self.control_panel, bg=colors.grey, highlightthickness=0)
         self.angle_container.place(anchor="n", relx=0.5, rely=0.55, relwidth=1, relheight=0.225)
 
         self.angle_label = tk.Label(self.angle_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Angle : ")
-        self.angle_label.place(anchor="center", relx=0.25, rely=0.3)
+        self.angle_label.place(anchor="center", relx=0.25, rely=0.5)
 
         self.angle_entry = tk.Entry(self.angle_container, font=text.P2, fg=colors.grey, bg=colors.white, relief="flat", justify="center")
-        self.angle_entry.place(anchor="center", relx=0.525, rely=0.3, relwidth=0.35, relheight=0.325)
+        self.angle_entry.place(anchor="center", relx=0.525, rely=0.5, relwidth=0.35, relheight=0.325)
 
         self.angle_unit = tk.Label(self.angle_container, font=text.P1, fg=colors.white, bg=colors.grey, text="deg")
-        self.angle_unit.place(anchor="center", relx=0.75, rely=0.3)
-
-        self.angle_decrease_button = tk.Button(self.angle_container, font=text.P2, fg=colors.grey, bg=colors.white, text="<")
-        self.angle_decrease_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.angle_decrease_button.place(anchor="center", relx=0.38, rely=0.7, relwidth=0.075)
-
-        self.angle_increase_button = tk.Button(self.angle_container, font=text.P2, fg=colors.grey, bg=colors.white, text=">")
-        self.angle_increase_button.config(relief="flat", activebackground=colors.grey, activeforeground=colors.white, cursor="hand2")
-        self.angle_increase_button.place(anchor="center", relx=0.67, rely=0.7, relwidth=0.075)
+        self.angle_unit.place(anchor="center", relx=0.75, rely=0.5)
 
         self.gravity_container = tk.Canvas(self.control_panel, bg=colors.grey, highlightthickness=0)
         self.gravity_container.place(anchor="n", relx=0.5, rely=0.775, relwidth=1, relheight=0.225)
 
-        self.gravity_label = tk.Label(self.gravity_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Gravity Preset : ")
-        self.gravity_label.place(anchor="center", relx=0.25, rely=0.3)
+        self.gravity_label = tk.Label(self.gravity_container, font=text.P1, fg=colors.white, bg=colors.grey, text="Gravity : ")
+        self.gravity_label.place(anchor="center", relx=0.25, rely=0.5)
 
         self.gravity_options = ["Earth (9.81)"]
         self.gravity_selected = tk.StringVar()
         self.gravity_selected.set(self.gravity_options[0])
-        self.gravity_selection_box= ttk.Combobox(self.gravity_container, value=self.gravity_options, font=text.P1)
+        self.gravity_selection_box= ttk.Combobox(self.gravity_container, value=self.gravity_options, font=text.P1, width=13, cursor="hand2")
         self.gravity_selection_box.config(state="readonly", textvariable=self.gravity_selected)
-        self.gravity_selection_box.place(anchor="center", relx=0.5, rely=0.7)
+        self.gravity_selection_box.current(0)
+        self.gravity_selection_box.place(anchor="center", relx=0.525, rely=0.5)
+
+        self.gravity_unit = tk.Label(self.gravity_container, font=text.P1, fg=colors.white, bg=colors.grey, text="m/s^2")
+        self.gravity_unit.place(anchor="center", relx=0.75, rely=0.5)
 
 
     def create_command_panel(self) -> None:
         self.command_panel = tk.Canvas(self.bot_panel, bg=colors.grey, highlightthickness=3)
         self.command_panel.place(anchor="sw", relx=0.025, rely=0.95, relwidth=0.325, relheight=0.15)
 
+        self.play_image_inactive = ImageTk.PhotoImage(Image.open("Play_Inactive.jpg").resize((70, 70)))
+        self.play_image_active = ImageTk.PhotoImage(Image.open("Play_Active.jpg").resize((70, 70)))
+        self.play_image_box = tk.Label(self.command_panel, bg=colors.grey, image=self.play_image_inactive, cursor="hand2")
+        self.play_image_box.image = self.play_image_inactive
+        self.play_image_box.place(anchor="center", relx=0.2, rely=0.5)
+        #self.play_image_box.bind("<Button-1>", lambda event: )
+
+        self.clear_image_inactive = ImageTk.PhotoImage(Image.open("Clear_Inactive.jpg").resize((70, 70)))
+        self.clear_image_active = ImageTk.PhotoImage(Image.open("Clear_Active.jpg").resize((70, 70)))
+        self.clear_image_box = tk.Label(self.command_panel, bg=colors.grey, image=self.clear_image_inactive, cursor="hand2")
+        self.clear_image_box.image = self.clear_image_inactive
+        self.clear_image_box.place(anchor="center", relx=0.5, rely=0.5)
+        #self.clear_image_box.bind("<Button-1>", lambda event: pass)
+
+        self.delete_image_inactive = ImageTk.PhotoImage(Image.open("Delete_Inactive.jpg").resize((70, 70)))
+        self.delete_image_active = ImageTk.PhotoImage(Image.open("Delete_Active.jpg").resize((70, 70)))
+        self.delete_image_box = tk.Label(self.command_panel, bg=colors.grey, image=self.delete_image_inactive, cursor="hand2")
+        self.delete_image_box.image = self.delete_image_inactive
+        self.delete_image_box.place(anchor="center", relx=0.8, rely=0.5)
+        #self.delete_image_box.bind("<Button-1>", lambda event: pass)
+
     def create_display_panel(self) -> None:
-        self.display_panel = tk.Canvas(self.bot_panel, bg=colors.grey, highlightthickness=3)
+        self.display_panel = tk.Canvas(self.bot_panel, bg=colors.grey, highlightthickness=0)
         self.display_panel.place(anchor="e", relx=0.975, rely=0.5, relwidth=0.6, relheight = 0.9)
 
 
